fccbff9cf3322fc955cb44a721ad3b761f1b9136
wangshuang <sxit@sxittekiMac-mini.local>
2013-11-13 23:29:23 +0800

    showmethemoney

---
+++ campus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  campus
+//
+//  Created by wangshuang on 11/13/13.
+//  Copyright (c) 2013 campus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ campusTests/campusTests.m
@@ -0,0 +1,34 @@
+//
+//  campusTests.m
+//  campusTests
+//
+//  Created by wangshuang on 11/13/13.
+//  Copyright (c) 2013 campus. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface campusTests : XCTestCase
+
+@end
+
+@implementation campusTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
